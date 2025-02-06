4df54ea336b0c3ecc6c910f8e4d15ce8ec42fae8
Bhargav Pratim Sharma <89238556+bhargav1131@users.noreply.github.com>
2025-02-06 20:01:45 +0530

    add function to fetch and set interview details by ID

--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+"use client"
+import React, {useEffect, useState} from 'react'
+import {db} from "@/utils/db"
+import {MockInterview} from "@/utils/schema"
+import {eq} from 'drizzle-orm'
 
 function StartInterview({params}) {
-    useEffect(()=>{
+    const [inerviewData, setInterviewData] = useState();
+    const [MockInterviewQuestion, setMockInterviewQuestion] = useState();
 
+    useEffect(()=>{
+        GetInterviewDetails();
     },[])
 
+    // used to get interview details by interview id
+    const GetInterviewDetails=async () => {
+        const result = await db.select().from(MockInterview).where(eq(MockInterview.mockId, params.interviewId));
+
+        const jsonMockResp=JSON.parse(result[0].jsonMockResp);
+        console.log(jsonMockResp);
+        setMockInterviewQuestion(jsonMockResp);
+        setInterviewData(result[0]);
+    }
 
   return (
-    <div>StartInterview</div>
+    <div>
+        <div>
+            
+        </div>
+    </div>
   )
 }
 
