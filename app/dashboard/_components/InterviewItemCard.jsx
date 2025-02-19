630511f06c48f22dcb2de456451dd87846824d39
Bhargav Pratim Sharma <89238556+bhargav1131@users.noreply.github.com>
2025-02-19 19:50:26 +0530

    feat: add navigation handlers for interview start and feedback pages

--- app/dashboard/_components/InterviewItemCard.jsx
+++ app/dashboard/_components/InterviewItemCard.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import {Button} from '@/components/ui/button';
+import {useRouter} from 'next/navigation';  
 
 function InterviewItemCard({interview}) {
+    const router = useRouter();
 
+    const onStart=()=>{
+        router.push('/dashboard/interview/'+interview?.mockId);
+    }
+
+    const OnFeedback=()=>{
+        router.push('/dashboard/interview/'+interview?.mockId+'/feedback');
+    }
 
     return (
         <div className='border shadow-sm rounded-lg p-3'>
@@ -11,8 +20,8 @@ function InterviewItemCard({interview}) {
             <h2 className='text-xs text-gray-500'>Created At: {interview.createdAt}</h2>
 
             <div className='flex justify-between mt-3 my-2 gap-5'>
-                <Button size='sm' variant='outline' className='w-full'>Feedback</Button>
-                <Button size='sm' className='w-full'>Start</Button>
+                <Button size='sm' variant='outline' className='w-full' onClick={OnFeedback}>Feedback</Button>
+                <Button size='sm' className='w-full' onClick={onStart}>Start</Button>
             </div>
         </div>
     )
